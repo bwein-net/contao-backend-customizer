117484b5f437116572e134dab13fda8f5e1e54ad
Ralf Baussenwein <ralf@bwein.net>
2021-04-03 18:28:36 +0200

    Update ecs config

--- ecs.php
+++ ecs.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
+use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
+use SlevomatCodingStandard\Sniffs\TypeHints\DisallowArrayTypeHintSyntaxSniff;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
@@ -13,18 +15,31 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         $vendorDir = __DIR__.'/../../vendor';
     }
 
-    $containerConfigurator->import($vendorDir.'/contao/easy-coding-standard/config/default.php');
+    $containerConfigurator->import($vendorDir.'/contao/easy-coding-standard/config/self.php');
 
     $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::EXCLUDE_PATHS, ['*/templates/*']);
-    $parameters->set(Option::CACHE_DIRECTORY, sys_get_temp_dir().'/ecs_default_cache_backend_customizer');
+    $parameters->set(
+        Option::SKIP,
+        [
+            '*/templates/*.html5',
+            MethodChainingIndentationFixer::class => [
+                '*/DependencyInjection/Configuration.php',
+            ],
+            DisallowArrayTypeHintSyntaxSniff::class => [
+                '*Model.php',
+            ],
+        ]
+    );
 
     $services = $containerConfigurator->services();
     $services
         ->set(HeaderCommentFixer::class)
-        ->call('configure', [[
-            'header' => "This file is part of Backend Customizer for Contao Open Source CMS.\n\n(c) bwein.net\n\n@license MIT",
-        ]])
-    ;
+        ->call(
+            'configure',
+            [
+                [
+                    'header' => "This file is part of Backend Customizer for Contao Open Source CMS.\n\n(c) bwein.net\n\n@license MIT",
+                ],
+            ]
+        );
 };
